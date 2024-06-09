c7c39a604258c2c5be502aebd7444f6c9683897a
= <=>
2024-06-10 01:26:05 +0800

    添加一些注释

--- src/main/java/com/greatming/greatmingtest/GreatmingtestApplication.java
+++ src/main/java/com/greatming/greatmingtest/GreatmingtestApplication.java
@@ -3,6 +3,8 @@ package com.greatming.greatmingtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// import org.springframework.context.annotation.ComponentScan;
+// @ComponentScan(basePackages = "com.greatming")	// 扫描指定包下的bean等，默认是该注解所在的类的包，已在SpringBootApplication注解中集成
 @SpringBootApplication
 public class GreatmingtestApplication {
 
